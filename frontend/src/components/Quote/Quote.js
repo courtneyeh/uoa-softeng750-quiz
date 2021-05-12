86d725c87e1b1ab99853a4db1bc3dfa098890dd0
Courtney Hunter <courtney.hunter.nz@gmail.com>
2021-05-12 22:47:35 +1200

    Remove console log

--- frontend/src/components/Quote/Quote.js
+++ frontend/src/components/Quote/Quote.js
@@ -1,7 +1,6 @@
 import styles from './Quote.module.css'
 
 export default function Quote({ quote }) {
-  console.log(quote);
   return (
     <div className={styles.quoteSection}>
       <b>{quote?.quote}</b>
