86b9d7ddf8edfe9c7c1990ad8d2fd97a5ea7656c
Courtney Hunter <courtney.hunter.nz@gmail.com>
2021-05-13 14:56:54 +1200

    Fix import path for MainButton in test suite

--- frontend/src/components/__tests__/MainButton.test.js
+++ frontend/src/components/__tests__/MainButton.test.js
@@ -1,6 +1,6 @@
 import { render, fireEvent, screen } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
-import MainButton from '../../pages/HomePage/MainButton/MainButton';
+import MainButton from '../../components/MainButton/MainButton';
 
 const PRESSED_TEXT = 'Pressed';
 const NOT_PRESSED_TEXT = 'Not Pressed';
